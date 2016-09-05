c54a917f309bf027cb11c2f409e03a385c16e66f
Roux Rémy <roux.remy.m@gmail.com>
2016-09-05 17:28:31 +0200

    site desktop fini font icone a ajouter

--- Projet/js/app.js
+++ Projet/js/app.js
@@ -68,11 +68,13 @@ $(document).ready(function () {
         // A chaque fois que l'utilisateur va scroller (descendre la page)
         var y = $(this).scrollTop(); // On récupérer la valeur du scroll vertical
         //si cette valeur > à 200 on ajouter la class
-        if (y >= (200)) {
-            $('#navbar').addClass('fixed_navbar');
+        if (y >= (400)) {
+            $('.navbar').addClass('fixed_navbar');
+            $('.navbar li:last-of-type').addClass('li_show');
         } else {
             // sinon, on l'enlève
-            $('#navbar').removeClass('fixed_navbar');
+            $('.navbar').removeClass('fixed_navbar');
+            $('.navbar li:last-of-type').removeClass('li_show');
         }
     });
 
