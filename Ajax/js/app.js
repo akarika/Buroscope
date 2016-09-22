4e6f47336a30deaf8bd538badebb747a363afe0a
akarika <roux.remy.m@gmail.com>
2016-09-22 12:22:24 +0200

    ajax

--- Ajax/js/app.js
+++ Ajax/js/app.js
@@ -3,9 +3,18 @@ $(document).ready(function(){
         //stop l effet du a
         e.preventDefault();
         //recupére la valeur de l artibut href
-        var cible= $(this).attr('href');
+        var cible = $(this).attr('href');
         //charge dans section la page html qui est la valur de cible
-        $('section').load(cible);
-    });
+        var link = this;
+        //on copie le contenu du fichier ciblé dans la section #content
+        // et on lance une fonction callback pour ajouter une classe
+        $('section').load(cible, function () {
+            //alert('BANZAI!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+            //on retire la classe active a tout les liens
+            $('nav a').removeClass("active");
+            //on ajoute une clase au lien cliqué
+            $(link).addClass("active");
 
+        });
+    });
 });
